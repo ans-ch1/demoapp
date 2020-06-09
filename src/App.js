971ca3a7cfc6abec27fa5af0e62cdad772c9a16b
Ans Choudhary <ans.ch7300@gmail.com>
2020-06-09 15:08:36 +0500

    app.css update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Dinner from './dinner.js';
 function App() {
   return (
     <div className='App'>
-    {/* <Dinner></Dinner> */}
+    <Header className= 'App-header'>
     <Dinner dishName="Chicken karahi" sweetDish="Kheer" />
     <hr/>
     <Dinner dishName="nihari" sweetDish="rasmilai" />
     <hr/>
     <Dinner dishName="Chicken biryani" sweetDish="gulabjamun" />
     <hr/>
+    <Header/>
     </div>
   );
   
